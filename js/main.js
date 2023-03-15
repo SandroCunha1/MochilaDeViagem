e7ffceb8f7dc0f0d02b066a23d94e3e26d98d916
Sandro <sandromedeirosdacunha@gmail.com>
2023-03-15 01:11:29 -0300

    LocalStorage quase funcionando, e implementação da função atualiza elemento
    atualizaElemento vai fazer com que se o objeto que você quer botar na mochila já exista ele só atualize a quantidade e não adicione mais um objeto igual

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
 const form = document.getElementById("novoItem");   
 const lista = document.getElementById("lista")
-form,addEventListener("submit", (evento) => {
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+  
+itens.forEach( (element) => {
+    criaElemento(element);
+});
+
+form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    console.log(evento)
-    criaElemento(evento.target.elements["nome"].value, evento.target.elements["quantidade"].value)
+
+    const nome = evento.target.elements["nome"].value;
+    const quantidade = evento.target.elements["quantidade"].value
+
+    const existe = itens.find( element => element.nome === nome)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+       }
+       if(existe){
+       itemAtual.id = existe.id
+
+       atualizaElemento(itemAtual);
+       }else{
+        itemAtual.id = itens.length
+        criaElemento(itemAtual)
+        itens.push(itemAtual)
+       }
+       
+
+       localStorage.setItem("itens", JSON.stringify(itens))
+
+       form.reset();
 })
-function criaElemento(nome, quantidade){
+function criaElemento(item){
    const novoItem = document.createElement("li")
    novoItem.classList.add("item")
 
    const numeroItem = document.createElement("strong")
-   numeroItem.innerHTML = quantidade
-
+   numeroItem.innerHTML = item.quantidade
+   numeroItem.dataset.id = item.id
    novoItem.appendChild(numeroItem)
-   novoItem.innerHTML += nome
-
+   
+   novoItem.innerHTML += item.nome
 
    lista.appendChild(novoItem)
+
+}
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
